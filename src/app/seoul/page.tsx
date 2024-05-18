283a9dda01cf512dabf8b052236335827ebc4bb7
gyumong <rlaalsrb3559@naver.com>
2024-05-18 09:54:35 +0900

    fads

---
+++ src/app/seoul/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import Button from '@/shared/@common/Button/Button';
+import useCurrentPosition from '@/shared/hooks/useCurrentPosition';
+import useGeocode from '@/shared/hooks/useGeoCode';
+import SearchDistanceStep from '@/app/request-form/components/steps/search-distance';
+import StepContainer from '@/app/request-form/components/steps/step-container';
+
+const SeoulPage = () => {
+  return <div className="h-dvh bg-white"></div>;
+};
+
+export default SeoulPage;
